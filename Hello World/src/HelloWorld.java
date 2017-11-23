4be455a78e50856795fd1bdcf697182fb07af082
seoil <seoil@HH311-40>
2017-11-23 15:33:52 +0900

    add print line

--- Hello World/src/HelloWorld.java
+++ Hello World/src/HelloWorld.java
@@ -2,8 +2,8 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-
+		System.out.println("Hello World");
+		System.out.println("Hello Everyone");
 	}
 
 }
